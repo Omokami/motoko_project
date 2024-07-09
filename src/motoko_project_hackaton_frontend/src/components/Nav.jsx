0b7e6a8020d2e28fe767f0fd84d50e870cb5b7d3
omokami <yamamoto.19940127@gmail.com>
2024-07-09 15:23:50 -0600

    feat: make navigation header responsive

--- src/motoko_project_hackaton_frontend/src/components/Nav.jsx
+++ src/motoko_project_hackaton_frontend/src/components/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
         <div className="w-4/5 my-auto">
             <ul className="flex justify-evenly font-bold text-3xl text-blue-900">
                 <li><Link to='/'><img src="./aquanova.png" alt="AquaNova Home" className="w-24" /></Link></li>
-                <li className="lg:my-auto hidden lg:block"><Link to='/initial'>Initial Test</Link></li>
+                <li className="lg:my-auto hidden lg:block "><Link to='/initial'>Initial Test</Link></li>
                 <li className="lg:my-auto hidden lg:block"><Link to='/final'>Final Test</Link></li>
                 <li className="lg:my-auto hidden lg:block"><Link to='/test'>Test</Link></li>
                 <li className="lg:my-auto hidden lg:block"><Link to='/suggest'>Suggestions</Link></li>
